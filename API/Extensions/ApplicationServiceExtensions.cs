d9f66bf9e124dc0dfca2832dc872103071cbcf14
Vinay <tellvinay2003@yahoo.co.in>
2021-05-03 01:46:58 +0530

    USe of Extension methods to clean Startup

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,7 +1,30 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace API.Extensions
 {
-    public class ApplicationServiceExtensions
+    public static class ApplicationServiceExtensions
     {
-        
+        /// <summary>
+        /// We are injecting configuration as well as this will reduce the complexity of startup class's ConfigureService method.
+        ///  where we are configuring application level data
+        /// </summary>
+        /// <param name="IServiceCollection"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
     }
 }
--- API/Startup_Old_Version.cs
+++ API/Startup_Old_Version.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
-using API.Extensions;
 using API.Interfaces;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -22,34 +21,48 @@ using Microsoft.OpenApi.Models;
 
 namespace API
 {
-    // With Extension method implementation for Identity and application services
-    // We have created 2 separate classes ApplicationServiceExtensions AND IdentityServiceExtensions
-    // We create extension methods. for this, we need to make these 2 classes Static
-    // 
-    // For origina and default implementation, please refer to Startup_Old_Version.cs
-    public class Startup
+    public class Startup_Old_Version
     {
         private readonly IConfiguration _config;
-        public Startup(IConfiguration config)
+        public Startup_Old_Version(IConfiguration config)
         {
             _config = config;
         }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         //  ordering of service calls are not important in this method
         public void ConfigureServices(IServiceCollection services)
         {
-            // all service level configuration are moved to extension class named ApplicationServiceExtensions
-            services.AddApplicationServices(_config);
-            // Add controllers
-            services.AddControllers();
+            // life time of "AddScoped" is during a complete HTTP request where as .AddSingleton is for complete app cycle so not to
+            // appropriate for Token base request. And AddScoped is appropriate 
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+
             // Access to XMLHttpRequest at 'https://localhost:5001/api/users' from origin 'http://localhost:4200' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
             services.AddCors();
-            // Swagger settings
+            services.AddControllers();
             services.AddSwaggerGen(c =>
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" }));
-            // Identity services (like authentication, authorizations) are moved to extension class IDentityServiceExtensions
-            services.AddIdentityServices(_config);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => 
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
+                            
+                            //Issuer of the token is our API server
+                            ValidateIssuer = false,
+
+                            // Audience is our angular application
+                            ValidateAudience = false
+                        };
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
